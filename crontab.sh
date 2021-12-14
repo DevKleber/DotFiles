df0dd74490e9d751c71f674d0dd5fa262cb7a9a7
Kleber de Souza <kleber107@gmail.com>
2021-12-14 17:40:30 -0300

    Crontab kleber

--- crontab.sh
+++ crontab.sh
@@ -3,6 +3,7 @@ sudo crontab -l > crontab_new_sudo
 
 echo "@reboot /home/$USER/shellscripts/git/git.sh" >> crontab_new
 echo "0 */2 * * * /home/$USER/shellscripts/git/git.sh" >> crontab_new
+echo "0 */8 * * * /home/$USER/shellscripts/pc-.zshrc/run.sh" >> crontab_new
 crontab crontab_new
 
 echo "00 20 * * * /home/$USER/atalhos/atualiza_otimiza.sh >> /var/log/atualiza_otimiza.log 2>&1" >> crontab_new_sudo
