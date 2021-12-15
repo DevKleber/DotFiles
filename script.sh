2b670bd1df1da50c382a974bd1981581d00c1063
Kleber de Souza <kleber107@gmail.com>
2021-12-15 10:06:11 -0300

    dotfile

--- script.sh
+++ script.sh
@@ -8,27 +8,29 @@ pwd=$(pwd)
 DIRETORIO_DOWNLOADS="$HOME/Downloads/programas"
 mkdir $DIRETORIO_DOWNLOADS
 
-source flameshot.sh
-source slack.sh
-source vscode.sh
-source spotify.sh
-source insomnia.sh
-source dbeaver.sh
-source chrome.sh
-source node.sh
-source instalandoDeb.sh
-source peek.sh
-source qbittorrent.sh
+# source flameshot.sh
+# source slack.sh
+# source vscode.sh
+# source spotify.sh
+# source insomnia.sh
+# source dbeaver.sh
+# source chrome.sh
+# source node.sh
+# source instalandoDeb.sh
+# source peek.sh
+# source qbittorrent.sh
 
-source aptInstall.sh
+# source aptInstall.sh
 
-source composer.sh
-source laravel.sh
-# source wine.sh
-source posInstalacao.sh
+# source composer.sh
+# source laravel.sh
+# # source wine.sh
+# source posInstalacao.sh
 
-source crontab.sh
+# source crontab.sh
 
-source git.sh
-source angular.sh
+# source git.sh
+# source angular.sh
+
+source ohMyZsh.sh
 
