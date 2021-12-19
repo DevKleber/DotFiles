03f9d41f29f0afa0987bc3c1ec703df65fbc709f
Kleber de Souza <kleber107@gmail.com>
2021-12-19 18:00:14 -0300

    Crontab

--- menu.sh
+++ menu.sh
@@ -8,18 +8,21 @@ sudo touch ~/.local/share/desktop-directories/cinnamon-testes.directory
 # Icon=star 
 # Type=Directory" | sudo tee ~/.local/share/desktop-directories/cinnamon-testes.directory
 
-# sudo sed -i '/<Layout>/i <Menu> <Name>Testes</Name> <Directory>cinnamon-testes.directory</Directory> </Menu>' /etc/xdg/menus/cinnamon-applications.menu
+sudo sed -i '0,/</Menu>$/s//*************************************/' /etc/xdg/menus/cinnamon-applications.menu
+
+# sudo sed -i '0,// fuck' /etc/xdg/menus/cinnamon-applications.menu
+# <Menu> <Name>Testes</Name> <Directory>cinnamon-testes.directory</Directory> </Menu>' /etc/xdg/menus/cinnamon-applications.menu
 
 # falta essa parte funcionar
-# sudo sed -i '0,/<Menuname>/i <Menuname>Atalhos</Menuname>' /etc/xdg/menus/cinnamon-applications.menu
+# sudo sed -i '0,/<Menuname>/i <Menuname>Testes</Menuname>' /etc/xdg/menus/cinnamon-applications.menu
 
-sudo touch /usr/share/applications/Testes.desktop
+# sudo touch /usr/share/applications/Testes.desktop
 
-echo "[Desktop Entry]
-Name=php71
-Exec=/home/kleber/atalhos/php/php7.1.sh
-Comment=
-Terminal=true
-Icon=/home/kleber/.icons/download/php.png
-Type=Application
-Categories=Atalhos;"| sudo tee /usr/share/applications/Testes.desktop
+# echo "[Desktop Entry]
+# Name=php71
+# Exec=/home/kleber/atalhos/php/php7.1.sh
+# Comment=
+# Terminal=true
+# Icon=/home/kleber/.icons/download/php.png
+# Type=Application
+# Categories=Atalhos;"| sudo tee /usr/share/applications/Testes.desktop
