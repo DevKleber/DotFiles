ff6de2568e42275ba5fc664a6f4b078721cc2403
Kleber de Souza <kleber107@gmail.com>
2022-01-15 10:00:16 -0300

    Crontab

--- crontab.sh
+++ crontab.sh
@@ -5,7 +5,7 @@ echo "@reboot /home/$USER/shellscripts/git/git.sh" >> crontab_new
 echo "0 */2 * * * /home/$USER/shellscripts/git/git.sh" >> crontab_new
 crontab crontab_new
 
-echo "00 20 * * * /home/$USER/atalhos/atualiza_otimiza.sh >> /var/log/atualiza_otimiza.log 2>&1" >> crontab_new_sudo
+echo "00 20 * * * /home/$USER/shellscripts/scriptsToRun/run.sh >> /var/log/atualiza_otimiza.log 2>&1" >> crontab_new_sudo
 sudo crontab crontab_new_sudo
 
 rm crontab_new
