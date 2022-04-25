fb360e60bcf62337ec6a24c46edd4c4e45f9b145
Kleber de Souza <kleber107@gmail.com>
2022-04-25 18:45:31 -0300

    Crontab kleber

--- ulauncher.sh
+++ ulauncher.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+#Bug no ubuntu 
+# Ativando teclado de atalho
+# https://github.com/Ulauncher/Ulauncher/wiki/Hotkey-In-Wayland
+
+#Ativando focus 
+# Install package wmctrl (needed to activate app focus)
+# sudo apt install wmctrl
+
 sudo add-apt-repository ppa:agornostal/ulauncher && sudo apt update && sudo apt install ulauncher
-sudo apt install ./ulauncher_x.y.z_all.deb
 
 #adicionando tema do dracula. 
 git clone https://github.com/dracula/ulauncher.git ~/.config/ulauncher/user-themes/dracula-ulauncher
