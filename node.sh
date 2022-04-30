4e2933968e8acdbe5c8b58fd2044a42a9c579c0f
Kleber de Souza <kleber107@gmail.com>
2022-04-30 10:19:42 -0300

    Crontab kleber

--- node.sh
+++ node.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# Caso tenha problema com uma versão já instalada, vamos remover e instalar novamente. 
+# sudo rm /etc/apt/sources.list.d/nodesource.list /etc/apt/sources.list.d/nodesource.list.save
+# sudo apt --fix-broken install
+# sudo apt update
+# sudo apt remove nodejs
+# sudo apt remove nodejs-doc
+# sudo apt autoremove
+
+
 #Pegando ultima versão LTS do node
 content=`wget -O - https://nodejs.org/en/`
 versiontext=$(echo "$content" | grep -o -P '(?<=title="Download).*(?=LTS)')
