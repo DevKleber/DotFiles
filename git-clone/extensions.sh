3eb64ad628ed028cad31c23578901f65154bf2dc
Kleber de Souza <kleber107@gmail.com>
2022-05-01 15:43:50 -0300

    push

--- git-clone/extensions.sh
+++ git-clone/extensions.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 DIR_VS_EXT="/home/${USER}/.vscode"
 FOLDER_VS_EXT="extensions"
+FOLDER_VS_ORIGINAL="extensions_original"
 PATH_VS_EXT="$DIR_VS_EXT/$FOLDER_VS_EXT"
+PATH_VS_EXT_ORIGINAL="$DIR_VS_EXT/$FOLDER_VS_ORIGINAL"
 
-if [ -d "$PATH_VS_EXT" ]
+if [ -d "$PATH_VS_EXT_ORIGINAL" ]
 then 
-    echo "${PATH_VS_EXT} ja existe"
-    sudo mv $PATH_VS_EXT "$DIR_VS_EXT/extensionsOld"
-    cd $DIR_VS_EXT && git clone git@github.com:DevKleber/pc-vscode-extensions.git $FOLDER_VS_EXT
+    echo "${PATH_VS_EXT_ORIGINAL} ja existe"
 else 
-    cd $DIR_VS_EXT && git clone git@github.com:DevKleber/pc-vscode-extensions.git $FOLDER_VS_EXT
+    if [ -d "$PATH_VS_EXT" ]
+    then 
+        echo "${PATH_VS_EXT} ja existe"
+        sudo mv $PATH_VS_EXT "$DIR_VS_EXT/$FOLDER_VS_ORIGINAL"
+        cd $DIR_VS_EXT && git clone git@github.com:DevKleber/pc-vscode-extensions.git $FOLDER_VS_EXT
+    else 
+        cd $DIR_VS_EXT && git clone git@github.com:DevKleber/pc-vscode-extensions.git $FOLDER_VS_EXT
+    fi
 fi
