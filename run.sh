d3b9e5bf0af4e872103b1e06756516cb456b17e3
Kleber de Souza <kleber107@gmail.com>
2022-08-07 10:56:34 -0300

    Crontab

--- run.sh
+++ run.sh
@@ -9,39 +9,39 @@ DIRETORIO_DOWNLOADS="$HOME/Downloads/programas"
 mkdir $DIRETORIO_DOWNLOADS
 
 # pacotes .deb
-source chrome.sh
-source dbeaver.sh
-source flameshot.sh
-source insomnia.sh
-source slack.sh
-source vscode.sh
-source instalandoDeb.sh
+# source chrome.sh
+# source dbeaver.sh
+# source flameshot.sh
+# source insomnia.sh
+# source slack.sh
+# source vscode.sh
+# source instalandoDeb.sh
 
-source spotify.sh
-source node.sh
-source peek.sh
-source qbittorrent.sh
+# source spotify.sh
+# source node.sh
+# source peek.sh
+# source qbittorrent.sh
 
-source aptInstall.sh
+# source aptInstall.sh
 
 # source php-versions.sh
 
 # source composer.sh
 # source laravel.sh
 
-source posInstalacao.sh
+# source posInstalacao.sh
 
-source crontab.sh
+# source crontab.sh
 
-source git.sh
+# source git.sh
 
-source fonts.sh
+# source fonts.sh
 
-source menu.sh
+# source menu.sh
 
 #source angular.sh
 # source docker.sh
-#source ohMyZsh.sh
+source ohMyZsh.sh
 
 #source android.sh
 
